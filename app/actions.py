40e9910e24c1eedf9269f242515369f9800cdcd2
Hiren Pandya <hiren@atsspec.com>
2019-07-17 11:53:56 -0400

    - Adding Vultr API - Prepare Vultr API Actions - Add another route in flask /vustat

--- app/actions.py
+++ app/actions.py
@@ -1,6 +1,7 @@
 import time
 from config import get_env
 from app.utils.dohelper import DoHelper
+from app.utils.vuhelper import VuHelper
 from app.utils.slackhelper import SlackHelper
 
 
@@ -11,10 +12,11 @@ class Actions:
         self.user_info = user_info
         self.slackhelper = slackhelper
         self.dohelper = DoHelper()
+        self.vuhelper = VuHelper()
 
     ''' [/dostat help] will call this function. It shall display all the available commands the bot can handle '''
 
-    def help(self):
+    def help_do(self):
         return {
             'text': 'Available Commands: \n `/dostat droplets  e.g. /dostat droplets` \n To get all available droplets`\n'
             ' \n `/dostat restart [droplet_name] e.g. /dostat restart nkn-droplet-client1` \n Restart mentioned droplet \n'
@@ -49,3 +51,41 @@ class Actions:
         ''' call slack api to post the message to the user on slack '''
         self.slackhelper.post_message(text_details)
         return {'text': 'Command completed successfully. \n \n DigitalOcean Bot 1.1'}
+
+    ''' [/vustat help] will call this function. It shall display all the available commands the bot can handle '''
+
+    def help_vu(self):
+        return {
+            'text': 'Available Commands: \n `/vustat instances  e.g. /vustat instances` \n To get all available instances`\n'
+            ' \n `/vustat restart [instance_label] e.g. /vustat restart nkn-vultr-client1-ny` \n Restart mentioned instance \n'
+                    ' \n `/vustat status [instance_label] e.g. /vustat status nkn-vultr-client1-ny` \n Display the status of mentioned instance \n'
+                    ' \n `/vustat help` \n Displays this information \n \n DigitalOcean Bot 1.2'}
+
+    ''' [/vustat instances] command will call this function. It will gather all the available
+        instances and display their label, ipaddresses, and current status in a table'''
+
+    def instances(self):
+        ''' Call vultr function to gather the data '''
+        text_details = self.vuhelper.get_instances()
+        ''' call slack api to post the message to the user on slack '''
+        self.slackhelper.post_message(text_details)
+        return {'text': 'Command completed successfully. \n \n DigitalOcean Bot 1.2'}
+
+    ''' [/vustat restart {instance_label}] command will call this function. It will restart the mentioned instance
+        single instance at the moment. If no instance label given, bot will show an error with appropriate message '''
+
+    def restart_instance(self, instance_label):
+        ''' Call vultr function to restart the droplet '''
+        text_details = self.vuhelper.instance_reboot(instance_label)
+        ''' call slack api to post the message to the user on slack '''
+        self.slackhelper.post_message(text_details)
+        return {'text': 'Command completed successfully. \n \n DigitalOcean Bot 1.2'}
+
+    ''' [/vustat status {instance_label}] command will call this function and display individual instance's status '''
+
+    def inst_status(self, instance_label):
+        ''' Call vultr function to gather the data '''
+        text_details = self.vuhelper.instance_status(instance_label)
+        ''' call slack api to post the message to the user on slack '''
+        self.slackhelper.post_message(text_details)
+        return {'text': 'Command completed successfully. \n \n DigitalOcean Bot 1.2'}
